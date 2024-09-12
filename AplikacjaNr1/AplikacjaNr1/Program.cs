cb6ccdbf275a78bc9a33d452de4d10022fef0f52
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2024-09-12 16:27:42 +0200

    Add files via upload

---
+++ AplikacjaNr1/AplikacjaNr1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaNr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Witaj 3ct");
+           // Console.WriteLine("Witaj 3ct");
+
+           // Console.Write("witaj 3 tc" + Convert.ToChar(13) + Convert.ToChar(10));
+            //Console.Write("hgfjgfhghfg" + Convert.ToChar(0));
+
+            while (true)
+            {
+                Console.Write(DateTime.Now.ToString() + Convert.ToChar(13));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
