b87a40825bfaf22281496be88686aea2f5687e0c
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2024-10-10 16:26:01 +0200

    Add files via upload

---
+++ 10.10.24/10.10.2024v1/10.10.2024v1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace _10._10._2024v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lekcja programowania");
+
+            var ini = new configFile(@".\config.ini");
+            string wartosc_parametru;
+
+            wartosc_parametru = ini.Read("ipAdress", "Drukarka");
+            Console.WriteLine("adres Ip = "+ wartosc_parametru);
+
+            wartosc_parametru = ini.Read("port", "Drukarka");
+            Console.WriteLine("port = " + wartosc_parametru);
+
+            string nazwa_drukarki;
+            wartosc_parametru = ini.Read("nazwa_drukarki", "Drukarka");
+            if(string.IsNullOrEmpty(wartosc_parametru))
+            {
+                nazwa_drukarki = "default_name";
+                ini.Write("nazwa_rukarki", nazwa_drukarki, "Drukarka");
+            } else if(!string.IsNullOrEmpty(wartosc_parametru))
+            {
+                nazwa_drukarki = wartosc_parametru;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+    public class configFile
+    {
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+
+        static extern long GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size,
+            string FilePath);
+
+        public string Read(string Key, string Section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+
+        public configFile(string IniPath=null)
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+        }
+
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+
+        }
+        public void DeleteSection(string Section = null)
+        {
+            Write(null, null, Section ?? EXE);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
---
+++ 10.10.24/10.10.2024v1/10.10.2024v1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("10.10.2024v1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10.10.2024v1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("7e142be0-1b47-44b3-bcba-1c299478c86b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
