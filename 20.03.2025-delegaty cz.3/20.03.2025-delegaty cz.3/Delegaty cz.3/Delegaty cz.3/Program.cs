beb782bb93cefede5bb7da2769d3268906f9c230
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2025-04-03 13:28:34 +0200

    Add files via upload

---
+++ 20.03.2025-delegaty cz.3/20.03.2025-delegaty cz.3/Delegaty cz.3/Delegaty cz.3/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+//dodaj(5, 5);
+//odejmij(5,5);
+
+//Niejawny delegat
+
+oblicz(5,5,dodaj);
+oblicz(7,7,odejmij);
+oblicz(7,7,mnozenie);
+oblicz(7,7,dzielenie);
+
+loguj("Brak dostępu do bazy danych.", logujDoPliku);
+loguj("Brak dostępu do bazy danych.", logujSmsem);
+void dodaj(int l1, int l2)
+{
+    Console.WriteLine(l1+l2);
+}
+
+void odejmij(int l1, int l2)
+{
+    Console.WriteLine(l1-l2);   
+}
+
+void mnozenie(int l1, int l2)
+{
+    Console.WriteLine(l1 * l2);
+}
+
+void dzielenie(int l1, int l2)
+{
+    Console.WriteLine(l1 / l2);
+}
+
+void oblicz(int l1, int l2, Action<int,int> operacja)
+{
+    operacja(l1,l2);
+}
+
+void logujDoPliku(string wiadomosc)
+{
+    Console.WriteLine($"Loguje do pliku wiadomosc {wiadomosc}");
+}
+void logujDoBazy(string wiadomosc)
+{
+    Console.WriteLine($"Loguje do bazy wiadomosc {wiadomosc}");
+}
+void logujSmsem(string wiadomosc)
+{
+    Console.WriteLine($"Loguje do smsem wiadomosc {wiadomosc}");
+}
+
+void loguj(string wiadomosc, Action<string> zaloguj)
+{
+    zaloguj(wiadomosc);
+}
---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.WriteLine(SilniaIt(5));
+Console.WriteLine(SilniaRe(5));
+double SilniaIt(long n)
+{
+    long wynik = 1;
+    for(int i =1; i<=n;i++)
+    {
+        wynik = wynik * i;
+
+    }
+    return wynik;
+}
+
+double SilniaRe(long n)
+{
+    if(n == 0)
+        return 1;
+
+    return n * SilniaRe(n - 1);
+}
+/** Dla SilniaRe(3)
+ * 
+ * Wywołanie: SilniaRe(3)--> n=3
+ * czy n==0? --> NIE
+ *  return SilniaRe(2)--> n==2
+ *   czy n==0? --> NIE
+ *    return SilniaRe(1)--> n==1
+ *     czy n==0? --> NIE
+ *      return SilniaRe(0)--> n==0
+ *       czy n==0? --> TAK
+ *        return 1
+ *       return 1*1 --> 1
+ *      return 2*1 --> 2
+ *     return 3*2 --> 6
+ *    return 6
